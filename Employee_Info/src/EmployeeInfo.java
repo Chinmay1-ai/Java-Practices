6cabb71803efaeff03454a0992a6c6cef99df16b
Chinmay1-ai <pawarchinmay92@gmail.com>
2025-10-31 15:56:35 +0530

    Add files via upload

---
+++ Employee_Info/src/EmployeeInfo.java
@@ -0,0 +1,15 @@
+public class EmployeeInfo {
+public static void main(String[] args) {
+	String Employee_Name = "Alex";
+	int Employee_ID = 12146;
+	float Salary = 50000f;
+	int Age = 30;
+	long Mob_No = 1234567890l;
+	
+	System.out.println("The Name of the Employee is "+Employee_Name);
+	System.out.println("The ID of the Employee is "+Employee_ID);
+	System.out.println("The Salary of the Employee is "+Salary);
+	System.out.println("The Age of the Employee is "+Age);
+	System.out.println("The Contact Number of the Employee is "+Mob_No);
+}
+}
