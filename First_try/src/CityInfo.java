5d663df8153b5c1b90b3eb3dde793c5415250a39
Chinmay1-ai <pawarchinmay92@gmail.com>
2025-11-01 07:00:10 +0530

    Add files via upload

---
+++ First_try/src/CityInfo.java
@@ -0,0 +1,9 @@
+public class CityInfo {
+public static void main(String[] args) {
+	System.out.println("My Favourite City is Pune");
+	System.out.println("I Like Pune because of it's Tradition");
+	System.out.println("My Favourite Place in Pune is Shaniwar wada");
+	System.out.println("I am Living in Pune From Past 2 Years");
+	System.out.println("My most of the Relatives lives in Pune");
+}	
+}
---
+++ First_try/src/ParentInfo.java
@@ -0,0 +1,9 @@
+public class ParentInfo {
+public static void main(String[] args) {
+	System.out.println("My Father is an Farmer");
+	System.out.println("My Mother is Housewife");
+	System.out.println("The age of my Father is 49");
+	System.out.println("The age of my Mother is 52");
+	System.out.println("My Parents Married in 1995");
+}
+}
