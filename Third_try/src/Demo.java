7f32674a53f561acb3ee8b942f8ca8123934a76c
Chinmay1-ai <pawarchinmay92@gmail.com>
2025-11-01 07:01:25 +0530

    Add files via upload

---
+++ Third_try/src/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args) {
+        int age = 21;
+        char email = '@';
+        float marks = 79.86F;
+        double percentage = 85.45;
+        boolean answer = true;
+        long mobNo = 7263840040L;
+        System.out.println("Age="+age+" Email="+email+" Marks="+marks+" Percentage="+percentage+" Answer="+answer+" MobNo="+mobNo);
+    }
+}
