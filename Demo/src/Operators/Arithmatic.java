33229e873ac42bb7bd440783c7beafa47d076fd1
Chinmay1-ai <pawarchinmay92@gmail.com>
2025-11-10 11:57:40 +0530

    Add files via upload

---
+++ Demo/src/Operators/Arithmatic.java
@@ -0,0 +1,21 @@
+package Operators;
+
+public class Arithmatic {
+public static void main(String[] args) {
+	
+	int a = 10;
+	int b = 14;
+	
+	int c = 4;
+	int d = 26;
+	
+	int e = 70;
+	int f = 18;
+	int g = 5;
+	
+	System.out.println(a+b);
+	System.out.println(d-c);
+	System.out.println(e*c-e+f);
+	System.out.println(c%d/g);
+}
+}
