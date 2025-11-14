10e6b3ba6edee68d6d5159f70be62b2859d50440
Chinmay1-ai <pawarchinmay92@gmail.com>
2025-11-14 08:59:15 +0530

    Add files via upload

---
+++ IF_Else_Test/src/com/if_else/Q2.java
@@ -0,0 +1,16 @@
+
+
+//Check whether a number is Even or Odd
+
+package com.if_else;
+
+public class Q2 {
+public static void main(String[] args) {
+	 int n = 9;
+     if(n % 2 == 0) {
+         System.out.println("Even");
+     }else {
+         System.out.println("Odd");
+     }
+}
+}
