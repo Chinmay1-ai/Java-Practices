d2b6faaa5c5128daeda4429f867a0b09ba3ffdfc
Chinmay1-ai <pawarchinmay92@gmail.com>
2025-11-16 15:55:46 +0530

    Add files via upload

---
+++ Operator_Test/src/com/operator/Question_2.java
@@ -0,0 +1,10 @@
+package com.operator;
+
+public class Question_2 {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 10;
+		int c = 15;
+		System.out.println((a < b) && (b < c));
+	}
+}
