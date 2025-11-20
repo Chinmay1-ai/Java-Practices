59d2983f67852458370b4753b22a399da9b6c3c2
Chinmay1-ai <pawarchinmay92@gmail.com>
2025-11-20 12:37:42 +0530

    Delete First_try directory

--- First_try/src/AnimalInfo.java
+++
@@ -1,9 +0,0 @@
-public class AnimalInfo {
-public static void main(String[] args) {
-	System.out.println("My Favourite Animal is Dog");
-	System.out.println("I also have one Dog whose name is Sunny");
-	System.out.println("The age of my Dog is 3 Years");
-	System.out.println("I also like Cat");
-	System.out.println("The Sunny is Siberian Husky");
-}
-}
