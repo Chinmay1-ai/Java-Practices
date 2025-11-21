e85214a68c25035ce7cacda73eb5b9cacf7978d7
Chinmay1-ai <pawarchinmay92@gmail.com>
2025-11-21 14:16:50 +0530

    Add files via upload

---
+++ Loops_Prac/src/com/prac/Q7.java
@@ -0,0 +1,21 @@
+//package com.prac;
+//
+//public class Q7 {
+//	public static void main(String[] args) {
+//		for (int i = 5; i <= 100; i++) {
+//			if (i % 5 == 0) {
+//				System.out.println(i);
+//			}
+//		}
+//	}
+//}
+
+package com.prac;
+
+public class Q7 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 20; i++) {
+			System.out.println(i * 5);
+		}
+	}
+}
---
+++ Loops_Prac/src/com/prac/Q9.java
@@ -0,0 +1,11 @@
+package com.prac;
+
+public class Q9 {
+	public static void main(String[] args) {
+		int i = 20;
+		while (i >= 1) {
+			System.out.println(i);
+			i--;
+		}
+	}
+}
