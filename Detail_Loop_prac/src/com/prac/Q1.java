8973a0c07db9b772966e3c5d1724c915a03443e1
Chinmay1-ai <pawarchinmay92@gmail.com>
2025-11-23 10:18:24 +0530

    Add files via upload

---
+++ Detail_Loop_prac/src/com/prac/Q1.java
@@ -0,0 +1,34 @@
+//package com.prac;
+//
+//public class Q1 {
+//	public static void main(String[] args) {
+//		for (int i = 1; i <= 100; i++) {
+//			System.out.println(i);
+//		}
+//	}
+//}
+
+//package com.prac;
+//
+//public class Q1 {
+//	public static void main(String[] args) {
+//		int i = 1;
+//		while (i <= 100) {
+//			System.out.println(i);
+//			i++;
+//		}
+//	}
+//}
+
+package com.prac;
+
+public class Q1 {
+	public static void main(String[] args) {
+
+		int i = 1;
+		do {
+			System.out.println(i);
+			i++;
+		} while (i <= 100);
+	}
+}
