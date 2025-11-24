f8edea7b07a36513d73021f902a694f7b35cb003
Chinmay1-ai <pawarchinmay92@gmail.com>
2025-11-24 09:28:41 +0530

    Add files via upload

---
+++ Switch_Try/src/com/sct/Q5.java
@@ -0,0 +1,25 @@
+package com.sct;
+
+public class Q5 {
+	public static void main(String[] args) {
+		char ch = 'B';
+
+		switch (ch) {
+
+		case 'A':
+		case 'E':
+		case 'I':
+		case 'O':
+		case 'U':
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u':
+			System.out.println("Vowel");
+
+		default:
+			System.out.println("Consonant");
+		}
+	}
+}
