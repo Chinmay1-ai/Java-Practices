41dd2dfba6d2428d70a28a6ba98f71da8500abca
Chinmay Pawar <pawarchinmay92@gmail.com>
2025-11-28 20:59:01 +0530

    Add files via upload

---
+++ oop/src/com/oop/Hospital.java
@@ -0,0 +1,14 @@
+package com.oop;
+
+public class Hospital {
+	public String hospitalName;
+	public String doctorName;
+	public  int doctorAge;
+	public  String specialization;
+	public  String patientName;
+	public  int patientAge;
+	public  String disease;
+	public  double fees;
+	public  boolean isAdmitted;
+	public  String city;
+}
