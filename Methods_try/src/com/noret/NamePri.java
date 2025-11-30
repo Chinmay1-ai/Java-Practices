88e326c2f36ddf53c0c410bcbb593ee54e1a264f
Chinmay Pawar <pawarchinmay92@gmail.com>
2025-11-30 10:15:34 +0530

    Add files via upload

---
+++ Methods_try/src/com/noret/NamePri.java
@@ -0,0 +1,22 @@
+//package com.noret;
+//
+//public class NamePri {
+//
+//	public String myName ;
+//	
+//	public void displayName() {
+//		System.out.println("Chinmay Pawar");
+//	}
+//}
+
+
+package com.noret;
+
+public class NamePri {
+
+	public String myName ;
+	
+	public void displayName(String myName) {
+		System.out.println("The Name of user is "+myName);
+	}
+}
