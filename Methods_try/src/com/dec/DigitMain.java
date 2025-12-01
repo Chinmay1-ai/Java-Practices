db5e00fc7609fe22e79ac293e0967a47ce2d5b8e
Chinmay Pawar <pawarchinmay92@gmail.com>
2025-12-01 16:47:54 +0530

    Add files via upload

---
+++ Methods_try/src/com/dec/DigitMain.java
@@ -0,0 +1,12 @@
+package com.dec;
+
+import com.noret.Digits;
+
+public class DigitMain {
+	public static void main(String[] args) {
+       
+		Digits digit = new Digits();
+		
+		digit.CountOdd(15697835);
+	}
+}
