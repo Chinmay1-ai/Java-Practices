2df364abbe1d419f03d9880490b449bee6d6bfb8
Chinmay Pawar <pawarchinmay92@gmail.com>
2025-12-03 12:44:03 +0530

    Add files via upload

---
+++ Methods_try/src/com/dec/NameMain.java
@@ -0,0 +1,27 @@
+//package com.dec;
+//
+//import com.noret.NamePri;
+//
+//public class NameMain {
+//	public static void main(String[] args) {
+//
+//		NamePri nam = new NamePri();
+//
+//		nam.displayName();
+//	}
+//}
+
+
+package com.dec;
+
+import com.noret.NamePri;
+
+public class NameMain {
+	public static void main(String[] args) {
+
+		NamePri nam = new NamePri();
+
+		nam.displayName("Chinmay Pawar");
+	}
+}
+
