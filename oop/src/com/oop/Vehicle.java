61b447ca4b85e52558010486947f940093679222
Chinmay Pawar <pawarchinmay92@gmail.com>
2025-12-08 23:21:41 +0530

    Add files via upload

---
+++ oop/src/com/oop/Vehicle.java
@@ -0,0 +1,16 @@
+package com.oop;
+
+public class Vehicle {
+
+	public String name;
+	public String type;
+	public int modelYear;
+	public String fuelType;
+	public int price;
+	public String company;
+	public int mileage;
+	public String color;
+	public int topSpeed;
+	public boolean isElectric;
+	
+}
