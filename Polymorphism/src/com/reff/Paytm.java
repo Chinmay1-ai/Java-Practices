7ea9a8ea314a883b1864bdd95cfae7e46c8259cc
Chinmay Pawar <pawarchinmay92@gmail.com>
2025-12-19 10:45:34 +0530

    Add files via upload

---
+++ Polymorphism/src/com/reff/Paytm.java
@@ -0,0 +1,14 @@
+package com.reff;
+
+public class Paytm extends Payment {
+
+	@Override
+	public void myBalance() {
+		System.out.println("Available Balance : 50,000");
+	}
+
+	@Override
+	public void myReward() {
+		System.out.println("You will get 10Rs Cashback on Paytm........");
+	}
+}
