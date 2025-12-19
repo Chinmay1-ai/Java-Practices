4acb3d053743ce17d62b1f22fbd994bc6c682caf
Chinmay Pawar <pawarchinmay92@gmail.com>
2025-12-19 10:43:51 +0530

    Add files via upload

---
+++ OOP_try/src/com/tka/Application.java
@@ -0,0 +1,41 @@
+package com.tka;
+
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		PersonalInfo p = new PersonalInfo();
+
+		System.out.println(".......What do you Want to Enter.......");
+		System.out.println("1. Name");
+		System.out.println("2. Age");
+		System.out.println("3. Salary");
+
+		int choice = sc.nextInt();
+		sc.nextLine();
+
+		if (choice == 1) {
+			System.out.println("Enter Name ");
+			String myName = sc.nextLine();
+			p.setName(myName);
+		} else if (choice == 2) {
+			System.out.println("Enter Age ");
+			int myAge = sc.nextInt();
+			p.setAge(myAge);
+		} else if (choice == 3) {
+			System.out.println("Enter thee Salary ");
+			double mySalary = sc.nextDouble();
+			p.setSalary(mySalary);
+		} else {
+			System.out.println("........Invalid Choice......");
+		}
+
+		System.out.println("...Personal Details...");
+		p.shoeDetail();
+		sc.close();
+
+	}
+}
