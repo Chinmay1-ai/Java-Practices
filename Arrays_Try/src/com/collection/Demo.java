9d367826639004b98dcda6aae855c118ae0a10f3
Chinmay Pawar <pawarchinmay92@gmail.com>
2026-01-18 07:52:41 +0530

    Add files via upload

---
+++ Arrays_Try/src/com/collection/Demo.java
@@ -0,0 +1,18 @@
+package com.collection;
+
+import java.util.ArrayList;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		ArrayList i = new ArrayList();
+
+		i.add("Chinmay");
+		i.add(21);
+		i.add("Male");
+		i.add("Pune");
+		i.add("Java Developer");
+		System.out.println(i);
+	}
+}
