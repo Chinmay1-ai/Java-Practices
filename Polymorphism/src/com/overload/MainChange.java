7f473f3dd32ab87741d8db54f0d0dd00c92a1a7f
Chinmay Pawar <pawarchinmay92@gmail.com>
2026-02-02 22:42:19 +0530

    Add files via upload

---
+++ Polymorphism/src/com/overload/MainChange.java
@@ -0,0 +1,12 @@
+package com.overload;
+
+public class MainChange {
+
+	public static void main(String[] args) {
+		System.out.println("--------------------------");
+	}
+
+	public static float main() {
+		return 2.2525f;
+	}
+}
