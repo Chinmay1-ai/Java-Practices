e1747214c933ce545419c0fcc60f31f5c2b43c22
Chinmay Pawar <pawarchinmay92@gmail.com>
2026-02-03 23:28:59 +0530

    Add files via upload

---
+++ Switch_Try/src/com/sct/Q7.java
@@ -0,0 +1,26 @@
+package com.sct;
+
+public class Q7 {
+	public static void main(String[] args) {
+		int season = 4;
+
+		switch (season) {
+
+		case 1:
+			System.out.println("Summer");
+			break;
+		case 2:
+			System.out.println("Rainy");
+			break;
+		case 3:
+			System.out.println("Winter");
+			break;
+		case 4:
+			System.out.println("Spring");
+			break;
+		default:
+			System.out.println("Invalid Choice of Season");
+			break;
+		}
+	}
+}
